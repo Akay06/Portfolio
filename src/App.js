7740696e9b0eec9c30a4b4b760428d83fb6aa27b
Akay06 <kanappa98@yahoo.com>
2024-10-17 22:03:05 -0500

    Added header styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./component/Header.js";
+import Home from "./component/Home.js";
+import Intro from "./component/Intro.js";
+import Skills from "./component/Skills.js";
+import Projects from "./component/Projects.js";
+import Contact from "./component/Contact.js";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/intro" element={<Intro />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -2,31 +2,48 @@ import React from "react";
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <nav className="navbar">
       <div className="navbar-top">
         <ul>
-          <li>K</li>
+          <li>
+            <NavLink to="/">K</NavLink>
+          </li>
         </ul>
       </div>
       <div className="navbar-container">
         <div className="navbar-center">
           <ul>
-            <li>Intro</li>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact</li>
+            <li>
+              <NavLink className="nav-link" to="/intro">
+                Intro
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/skills">Skills</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
           </ul>
         </div>
         <div className="navbar-bottom">
           <ul>
             <li>
-              <FontAwesomeIcon icon={faGithub} size="xl" />
+              <a href={process.env.REACT_APP_GITHUB_URL} target="_blank">
+                <FontAwesomeIcon icon={faGithub} size="xl" />
+              </a>
             </li>
             <li>
-              <FontAwesomeIcon icon={faLinkedin} size="xl" />
+              <a href={process.env.REACT_APP_LINKEDIN_URL} target="_blank">
+                <FontAwesomeIcon icon={faLinkedin} size="xl" />
+              </a>
             </li>
           </ul>
         </div>
